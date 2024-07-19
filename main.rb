5213010147bf0b0d53341b078711417722c3678a
Umair Ahmed <umairahmed@Umairs-Laptop.local>
2024-07-19 12:09:18 +0500

    Done with the add event feature

---
+++ main.rb
@@ -0,0 +1,54 @@
+require_relative "event.rb"
+require_relative "calendar.rb"
+require_relative "ui.rb"
+
+require 'date'
+
+correct_option=true
+loop do 
+	system(`clear`)
+	puts "\nCalender Menu"
+	puts "1. Add Event"
+    puts "2. Show Events List"
+    puts "3. Edit Event"
+	puts "4. Delete Event"
+    puts "5. Exit"
+	
+	correct_option ? puts("Choose Option") : puts("Choose correct option")
+
+	choice=gets.chomp.to_i
+
+	case choice
+	when 0
+		puts "Oops! You can only enter integer"
+	when 1
+        
+        date=UserInterface.instance.get_date
+
+        # Getting title
+        title=nil
+        loop do
+            print "Enter title: "
+            title=gets.chomp
+            if title.empty?
+                print "Title cannot be empty. Please enter a valid title: "
+            else
+                break
+            end
+        end
+
+        # Getting description of the event
+        print "Enter description: "
+        description=gets.chomp
+        
+
+        Calendar.instance.add_event(date,title,description)
+        puts "Event added successfully"
+        
+        sleep(2)
+    when 2
+        break
+    else
+		correct_option=false
+	end
+end
